85017c3ef9dd1ec59e6b23694abb217eb2c3c420
csauve <connorsauve@gmail.com>
2015-07-05 20:31:35 -0700

    Update deploy-hooks

--- deploy-hooks.sh
+++ deploy-hooks.sh
@@ -2,12 +2,12 @@ stage=$1
 
 case $stage in
   pre )
-    pm2 greenbot stop
+    pm2 stop greenbot
     ;;
   deploy )
     git pull
+    rm -r node_modules
     npm install
-    npm update
     ;;
   post )
     pm2 start node_modules/greenbot/bin/greenbot.js --node-args "modacity.cson"
